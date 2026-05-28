4502bebe266cc553853470cd342a15a279f4a605
voyageedtravel-hash <voyageedtravel@gmail.com>
2026-05-28 12:45:29 +0530

    feat: add service worker for offline support

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 've-crm-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() =>
+      caches.open(CACHE).then(c => c.match('/'))
+    ));
+  }
+});
