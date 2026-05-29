a6cd9c2a374fd3f0c07c7da625a49808ba5f264a
voyageedtravel-hash <voyageedtravel@gmail.com>
2026-05-29 10:51:36 +0530

    fix: rename to MainActivity WebView

--- android/app/src/main/java/com/voyageed/crm/WebViewFallbackActivity.java
+++ android/app/src/main/java/com/voyageed/crm/WebViewFallbackActivity.java
@@ -6,16 +6,29 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class WebViewFallbackActivity extends Activity {
+public class MainActivity extends Activity {
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://remarkable-horse-c2fed5.netlify.app/");
         setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
